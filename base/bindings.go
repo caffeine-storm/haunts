4ec475c6d29113578392abadcc217992f053e9b2
Thomas McKee <caffeine.storm@gmail.com>
2024-09-10 16:49:38 -0400

    fix: look for keybind identifiers case insensitively
    tdd: green

--- base/bindings.go
+++ base/bindings.go
@@ -24,7 +24,7 @@ func getKeysFromString(str string) []gin.KeyId {
   parts := strings.Split(str, "+")
   var kids []gin.KeyId
   for _, part := range parts {
-    part = osSpecifyKey(part)
+    part = strings.ToLower(osSpecifyKey(part))
     var kid gin.KeyId
     switch {
     case len(part) == 1: // Single character - should be ascii
