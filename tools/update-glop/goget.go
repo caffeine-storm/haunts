2ba5ce343d8e30ec2f58743cdd392d562d7848f2
Thomas McKee <caffeine.storm@gmail.com>
2024-10-11 15:19:25 -0400

    feat: add a ParseRev for parsing a revision from 'go get' output
    tdd: green

--- tools/update-glop/goget.go
+++ tools/update-glop/goget.go
@@ -1,7 +1,29 @@
 package updateglop
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
 func ParseRev(goGetCommandData string) (string, error) {
-	return "", fmt.Errorf("synth-error")
+	line, _, found := strings.Cut(goGetCommandData, ": parsing go.mod")
+	if !found {
+		return "", fmt.Errorf("couldn't strings.Cut input")
+	}
+
+	target := "glop@"
+	rev := line[strings.LastIndex(line, target)+len(target):]
+
+	// rev should look like 'v0.0.0-20241010144107-015f009dded2'
+	matched, err := regexp.Match(`v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+-[[:digit:]]{14}-[[:xdigit:]]{12}`, []byte(rev))
+	if err != nil {
+		panic(fmt.Errorf("regex error: %v", err))
+	}
+
+	if !matched {
+		panic(fmt.Errorf("input parsing failed to match %q against a version", rev))
+	}
+
+	return rev, nil
 }
