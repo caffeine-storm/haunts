53a16b78a3eeaf2d6cee8fe91b8442a233807b4e
Thomas McKee <caffeine.storm@gmail.com>
2024-10-22 14:11:58 -0400

    docs: add mention of cause of crashes
    tdd: refactor

--- house/house_viewer.go
+++ house/house_viewer.go
@@ -153,6 +153,8 @@ func (hv *HouseViewer) boardToModelview(mx, my float32) (x, y, z float32) {
 }
 
 func (hv *HouseViewer) WindowToBoard(wx, wy int) (float32, float32) {
+	// TODO(tmckee): this call will always fail; Room needs its embedded
+	// 'RoomDef'.
 	hv.floor, hv.ifloor, _, _, _, _ = makeRoomMats(&Room{}, hv.Render_region, hv.fx, hv.fy, hv.angle, hv.zoom)
 
 	fx, fy, _ := hv.modelviewToBoard(float32(wx), float32(wy))
@@ -160,6 +162,8 @@ func (hv *HouseViewer) WindowToBoard(wx, wy int) (float32, float32) {
 }
 
 func (hv *HouseViewer) BoardToWindow(bx, by float32) (int, int) {
+	// TODO(tmckee): this call will always fail; Room needs its embedded
+	// 'RoomDef'.
 	hv.floor, hv.ifloor, _, _, _, _ = makeRoomMats(&Room{}, hv.Render_region, hv.fx, hv.fy, hv.angle, hv.zoom)
 
 	fx, fy, _ := hv.boardToModelview(bx, by)
