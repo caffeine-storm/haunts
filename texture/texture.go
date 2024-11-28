a97008c787922f73f9e5906770a492817e5bad44
Thomas McKee <caffeine.storm@gmail.com>
2024-11-28 11:12:17 -0500

    fix: make the error texture not invisible
    tdd: refactor

--- texture/texture.go
+++ texture/texture.go
@@ -16,6 +16,6 @@ func makeErrorTexture() {
 	gl.TexParameterf(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
 	gl.TexParameterf(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
 	gl.TexParameterf(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
-	transparent := []byte{0, 0, 0, 0}
-	glu.Build2DMipmaps(gl.TEXTURE_2D, 4, 1, 1, gl.RGBA, transparent)
+	singleTexel := []byte{255, 0, 255, 255}
+	glu.Build2DMipmaps(gl.TEXTURE_2D, 4, 1, 1, gl.RGBA, singleTexel)
 }
