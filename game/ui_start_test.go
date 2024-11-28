41217387e41f10ef8392b52a2d4dc44c3f6cb028
Thomas McKee <caffeine.storm@gmail.com>
2024-11-28 11:28:00 -0500

    test: rewrite the startup rendertest to render as we'd expect
    tdd: green

--- game/ui_start_test.go
+++ game/ui_start_test.go
@@ -1,12 +1,16 @@
 package game_test
 
 import (
+	"fmt"
+	"path"
 	"testing"
 
 	"github.com/MobRulesGames/haunts/base"
 	"github.com/MobRulesGames/haunts/game"
+	"github.com/MobRulesGames/haunts/globals"
 	"github.com/MobRulesGames/haunts/texture"
 	"github.com/runningwild/glop/gui"
+	"github.com/runningwild/glop/gui/guitest"
 	"github.com/runningwild/glop/render"
 	"github.com/runningwild/glop/render/rendertest"
 	"github.com/runningwild/glop/system"
@@ -14,25 +18,20 @@ import (
 )
 
 func givenAStartMenu() *game.StartMenu {
-	return &game.StartMenu{}
+	parent := &gui.StandardParent{}
+	err := game.InsertStartMenu(parent)
+	if err != nil {
+		panic(fmt.Errorf("couldn't insert start menu: %w", err))
+	}
+	return parent.GetChildren()[0].(*game.StartMenu)
 }
 
-type stubContext struct{}
-
-func (*stubContext) GetDictionary(fontname string) *gui.Dictionary {
-	return nil
-}
-
-func (*stubContext) GetShaders(fontname string) *render.ShaderBank {
-	return nil
-}
-
-func givenADrawingContext(render.RenderQueueInterface) gui.DrawingContext {
-	return &stubContext{}
+func givenADrawingContext(dims gui.Dims) gui.UpdateableDrawingContext {
+	return guitest.MakeStubbedGui(dims)
 }
 
 func RunStartupSpecs() {
-	base.SetupLogger("../testdata")
+	base.SetDatadir("../data")
 	windowRegion := gui.Region{
 		Point: gui.Point{X: 0, Y: 0},
 		Dims:  gui.Dims{Dx: 1024, Dy: 750},
@@ -40,9 +39,39 @@ func RunStartupSpecs() {
 	menu := givenAStartMenu()
 
 	rendertest.WithGlForTest(windowRegion.Dx, windowRegion.Dy, func(sys system.System, queue render.RenderQueueInterface) {
+		queue.Queue(func(st render.RenderQueueState) {
+			globals.SetRenderQueueState(st)
+		})
+		queue.Purge()
+
+		ctx := givenADrawingContext(windowRegion.Dims)
+		base.InitDictionaries(ctx)
 		texture.Init(queue)
-		ctx := givenADrawingContext(queue)
-		menu.Draw(windowRegion, ctx)
+
+		startTexture := path.Join(base.GetDataDir(), "ui", "start", "start.png")
+		menuTexture := path.Join(base.GetDataDir(), "ui", "start", "menu.png")
+
+		var err error
+		_, err = texture.LoadFromPath(startTexture)
+		if err != nil {
+			panic(err)
+		}
+		_, err = texture.LoadFromPath(menuTexture)
+		if err != nil {
+			panic(err)
+		}
+		texture.BlockUntilLoaded(startTexture, menuTexture)
+
+		// Call .Think 56 times to 'shade in' the button text.
+		// TODO(tmckee): oh please god no!
+		for i := 0; i < 56; i++ {
+			menu.Think(nil, 42)
+		}
+
+		queue.Queue(func(st render.RenderQueueState) {
+			menu.Draw(windowRegion, ctx)
+		})
+		queue.Purge()
 
 		So(queue, rendertest.ShouldLookLikeFile, "startup")
 	})
