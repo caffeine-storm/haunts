f8bb6798d38363548ad40487ad646a18fd661025
Thomas McKee <caffeine.storm@gmail.com>
2024-12-12 14:42:37 -0500

    docs: explain why logtesting.CollectOutput is so nice
    tdd: refactor

--- logging/logtesting/collectoutput.go
+++ logging/logtesting/collectoutput.go
@@ -11,6 +11,8 @@ import (
 // what's needed to capture that output.
 func CollectOutput(fn func()) []string {
 	return gloptest.CollectOutput(func() {
+		// We can redirect to the current os.Stderr because that's what
+		// gloptest.CollectOutput will be collecting.
 		reset := logging.Redirect(os.Stderr)
 		defer reset()
 
