1512ca4705aaad95f40cc032148a8ed63bd1ea54
Thomas McKee <caffeine.storm@gmail.com>
2025-02-24 09:54:17 -0500

    test: verify that RoomViewer can Draw itself
    tdd: red

--- house/room_viewer_test.go
+++ house/room_viewer_test.go
@@ -4,9 +4,16 @@ import (
 	"math"
 	"testing"
 
+	"github.com/MobRulesGames/haunts/base"
 	"github.com/MobRulesGames/haunts/house"
+	"github.com/MobRulesGames/haunts/registry"
+	"github.com/MobRulesGames/haunts/texture"
 	"github.com/MobRulesGames/mathgl"
 	"github.com/runningwild/glop/gui"
+	"github.com/runningwild/glop/gui/guitest"
+	"github.com/runningwild/glop/render"
+	"github.com/runningwild/glop/render/rendertest"
+	"github.com/runningwild/glop/system"
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
 )
@@ -95,10 +102,31 @@ func TestMakeRoomMats(t *testing.T) {
 }
 
 func RoomViewerSpecs() {
-	room := GivenARoom("restest")
+	base.SetDatadir("../data")
 
-	Convey("can be made", func() {
-		rv := house.MakeRoomViewer(room, 0)
-		So(rv, ShouldNotBeNil)
+	rendertest.WithGlForTest(256, 256, func(sys system.System, queue render.RenderQueueInterface) {
+		registry.LoadAllRegistries()
+		base.InitShaders(queue)
+		texture.Init(queue)
+		room := loadRoom("restest.room")
+
+		Convey("can be made", func() {
+			rv := house.MakeRoomViewer(room, 0)
+			So(rv, ShouldNotBeNil)
+
+			Convey("can be drawn", func() {
+				twoFiveSixSquare := gui.Dims{
+					Dx: 256, Dy: 256,
+				}
+				reg := gui.Region{}
+				reg.Dims = twoFiveSixSquare
+				g := guitest.MakeStubbedGui(twoFiveSixSquare)
+				queue.Queue(func(render.RenderQueueState) {
+					rv.Draw(reg, g)
+				})
+
+				So(queue, rendertest.ShouldLookLikeFile, "room-viewer")
+			})
+		})
 	})
 }
