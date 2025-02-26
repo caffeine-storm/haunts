f86e0a6ce10a18e2dad432cde7ed8b7485bed03f
Thomas McKee <caffeine.storm@gmail.com>
2025-02-26 12:58:15 -0500

    fix(test): use logging.Redirect instead of glop.CollectOutput to intercept logs and add more brackets
    tdd: not-really

--- logging/bracket.go
+++ logging/bracket.go
@@ -1,10 +1,34 @@
 package logging
 
-import "log/slog"
+import (
+	"log/slog"
+
+	"github.com/runningwild/glop/glog"
+)
 
 // Run 'fn' in a context where log messages at 'lvl' and above are propagated.
 func Bracket(lvl slog.Level, fn func()) {
 	fixup := SetLoggingLevel(lvl)
 	defer fixup()
 	fn()
+}
+
+func ErrorBracket(fn func()) {
+	Bracket(slog.LevelError, fn)
+}
+
+func WarnBracket(fn func()) {
+	Bracket(slog.LevelWarn, fn)
+}
+
+func InfoBracket(fn func()) {
+	Bracket(slog.LevelInfo, fn)
+}
+
+func DebugBracket(fn func()) {
+	Bracket(slog.LevelDebug, fn)
+}
+
+func TraceBracket(fn func()) {
+	Bracket(glog.LevelTrace, fn)
 }
--- logging/bracket_test.go
+++ logging/bracket_test.go
@@ -1,25 +1,54 @@
 package logging_test
 
 import (
+	"bytes"
 	"log/slog"
-	"strings"
 	"testing"
 
 	"github.com/MobRulesGames/haunts/logging"
-	"github.com/runningwild/glop/gloptest"
+	"github.com/runningwild/glop/glog"
 	"github.com/stretchr/testify/assert"
 )
 
 const canary = "lololololololol"
 
 func TestLoggingBracket(t *testing.T) {
-	t.Run("at error should block trace", func(t *testing.T) {
-		loglines := gloptest.CollectOutput(func() {
-			logging.Bracket(slog.LevelError, func() {
-				logging.Trace(canary)
-			})
+	t.Run("at error blocks trace", func(t *testing.T) {
+		buf := &bytes.Buffer{}
+		fixup := logging.Redirect(buf)
+
+		logging.Bracket(slog.LevelError, func() {
+			logging.Trace(canary)
 		})
 
-		assert.NotContains(t, strings.Join(loglines, "\n"), canary)
+		fixup()
+
+		assert.NotContains(t, buf.String(), canary)
 	})
+
+	t.Run("at trace emits info", func(t *testing.T) {
+		buf := &bytes.Buffer{}
+		fixup := logging.Redirect(buf)
+
+		logging.Bracket(glog.LevelTrace, func() {
+			logging.Info(canary)
+		})
+
+		fixup()
+
+		assert.Contains(t, buf.String(), canary)
+	})
+}
+
+func TestTraceBracket(t *testing.T) {
+	buf := &bytes.Buffer{}
+	fixup := logging.Redirect(buf)
+
+	logging.TraceBracket(func() {
+		logging.Trace(canary)
+	})
+
+	fixup()
+
+	assert.Contains(t, buf.String(), canary)
 }
