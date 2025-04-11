5ea19c6e45e3d62638da5827519445dddda2f001
Thomas McKee <caffeine.storm@gmail.com>
2025-04-11 12:23:51 -0400

    test: we should be able to draw the versus game menu
    tdd: red

---
+++ game/ui_versus_test.go
@@ -0,0 +1,39 @@
+package game_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/MobRulesGames/haunts/base"
+	"github.com/MobRulesGames/haunts/game"
+	"github.com/MobRulesGames/haunts/game/gametest"
+	"github.com/MobRulesGames/haunts/logging"
+	"github.com/runningwild/glop/gui"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func panicInsteadOfReplace(x gui.WidgetParent) error {
+
+	panic(fmt.Errorf("panicInsteadOfReplace got called with %v", x))
+}
+
+func givenAVersusMenu() gametest.Drawer {
+	parent := &gui.StandardParent{}
+	err := game.InsertVersusMenu(parent, panicInsteadOfReplace)
+	if err != nil {
+		panic(fmt.Errorf("couldn't insert versus menu: %w", err))
+	}
+
+	return parent.GetChildren()[0].(gametest.Drawer)
+}
+
+func TestUiVersus(t *testing.T) {
+	Convey("UI for starting an online game", t, func() {
+		base.SetDatadir("../data")
+		versusScreen := givenAVersusMenu()
+
+		logging.DebugBracket(func() {
+			gametest.RunDrawingTest(versusScreen, "versus", func(gametest.DrawTestContext) {})
+		})
+	})
+}
