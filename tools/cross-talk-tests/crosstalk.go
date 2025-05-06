d508cd0a43f8815c4c9c16ecd9a3287a0a3d9dd5
Thomas McKee <caffeine.storm@gmail.com>
2025-05-06 09:45:25 -0400

    tools: add helper to isolate which tests cause cross-talk failure
    tdd: not-really

---
+++ tools/cross-talk-tests/crosstalk.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log/slog"
+	"math/rand/v2"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s <victim> <suite>\n", os.Args[0])
+	os.Exit(1)
+}
+
+func readTestList(fpath string) []string {
+	f, err := os.Open(fpath)
+	if err != nil {
+		panic(fmt.Errorf("couldn't os.Open %q: %w", fpath, err))
+	}
+
+	byteSlice, err := io.ReadAll(f)
+	if err != nil {
+		panic(fmt.Errorf("couldn't io.ReadAll %q: %w", fpath, err))
+	}
+
+	lines := bytes.Split(byteSlice, []byte{'\n'})
+	ret := []string{}
+	for _, line := range lines {
+		if len(line) != 0 {
+			ret = append(ret, string(line))
+		}
+	}
+
+	slog.Info("readTestList", "fpath", fpath, "result", ret)
+	return ret
+}
+
+func hasCrossTalk(victim, suite []string) bool {
+	// First, randomize the order of the suite so that we can explore different
+	// samplings than just contiguous slices of the original list.
+	rand.Shuffle(len(suite), func(i, j int) {
+		suite[i], suite[j] = suite[j], suite[i]
+	})
+
+	testpattern := strings.Join(append(victim, suite...), "\\|")
+	testrunargs := fmt.Sprintf("testrunargs=-run %s", testpattern)
+
+	// TODO: if we care, we can make the "run-the-tests" command a parameter.
+	cmd := exec.Command("make", testrunargs, "test-nocache")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	slog.Info("wut", "testrunargs", testrunargs, "cmd", cmd)
+	err := cmd.Run()
+	return err != nil
+}
+
+func showTest(parts []string) string {
+	return strings.Join(parts, ", ")
+}
+
+func reportCrossTalk(victim, suite []string) {
+	fmt.Printf("cross talk reduction: %q coupled with %q still fails\n", showTest(victim), showTest(suite))
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		usage()
+	}
+
+	victim := os.Args[1]
+	suite := os.Args[2]
+
+	victimTests := readTestList(victim)
+	suiteTests := readTestList(suite)
+
+	// Run 'suiteTests' and 'victimTests'
+	// if tests fail, assume it's because of cross talk
+	//   try again with first half of sweet
+	//		 fail => first-half again
+	//		 success => second-half
+
+	if !hasCrossTalk(victimTests, suiteTests) {
+		panic("couldn't repro with original specification")
+	}
+
+	for len(suiteTests) > 0 {
+		if len(suiteTests) == 1 {
+			reportCrossTalk(victimTests, suiteTests)
+			return
+		}
+		slog.Info("looping", "suiteTests", suiteTests)
+		suiteLen := len(suiteTests)
+
+		firstHalf, secondHalf := suiteTests[0:suiteLen/2], suiteTests[suiteLen/2:]
+
+		if hasCrossTalk(victimTests, firstHalf) {
+			suiteTests = firstHalf
+			continue
+		}
+
+		if hasCrossTalk(victimTests, secondHalf) {
+			suiteTests = secondHalf
+			continue
+		}
+
+		// We could try to sample differently but, for now, 'suiteTests' is a good
+		// candidate.
+		reportCrossTalk(victimTests, suiteTests)
+		return
+	}
+
+	panic("ran out of input!?")
+}
