1d4ed45664dcb2db652b8c84c8bea7b430c277a5
Thomas McKee <caffeine.storm@gmail.com>
2025-05-14 12:11:26 -0400

    test: a room viewer ought to be able to view a room
    tdd: red

--- house/room_viewer_test.go
+++ house/room_viewer_test.go
@@ -4,54 +4,18 @@ import (
 	"testing"
 
 	"github.com/MobRulesGames/haunts/base"
+	"github.com/MobRulesGames/haunts/game/gametest"
 	"github.com/MobRulesGames/haunts/house"
-	"github.com/MobRulesGames/haunts/logging"
-	"github.com/MobRulesGames/haunts/registry"
-	"github.com/MobRulesGames/haunts/texture"
-	"github.com/runningwild/glop/gui"
-	"github.com/runningwild/glop/gui/guitest"
-	"github.com/runningwild/glop/render"
-	"github.com/runningwild/glop/render/rendertest"
-	"github.com/runningwild/glop/render/rendertest/testbuilder"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestRoomViewer(t *testing.T) {
 	base.SetDatadir("../data")
 
-	screenRegion := gui.Region{
-		Point: gui.Point{
-			X: 0, Y: 0,
-		},
-		Dims: gui.Dims{
-			Dx: 256, Dy: 256,
-		},
-	}
 	Convey("house.roomViewer", t, func() {
-		testbuilder.New().WithSize(screenRegion.Dx, screenRegion.Dy).WithQueue().Run(func(queue render.RenderQueueInterface) {
-			registry.LoadAllRegistries()
-			base.InitShaders(queue)
-			texture.Init(queue)
+		gametest.RunDrawingTest(func() gametest.Drawer {
 			room := loadRoom("restest.room")
-
-			Convey("can be made", func() {
-				rv := house.MakeRoomViewer(room, 0)
-				So(rv, ShouldNotBeNil)
-
-				// TODO(#10): don't skip, fix!
-				SkipConvey("can be drawn", func() {
-					g := guitest.MakeStubbedGui(screenRegion.Dims)
-					g.AddChild(rv)
-					queue.Queue(func(render.RenderQueueState) {
-						logging.TraceBracket(func() {
-							g.Draw()
-						})
-					})
-					queue.Purge()
-
-					So(queue, rendertest.ShouldLookLikeFile, "room-viewer", rendertest.Threshold(0))
-				})
-			})
-		})
+			return house.MakeRoomViewer(room, 0)
+		}, "room-viewer")
 	})
 }
