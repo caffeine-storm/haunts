3ede31d4eca92a51d8bbf97edcce0f2251ad08e7
Thomas McKee <caffeine.storm@gmail.com>
2025-06-02 12:08:19 -0400

    test: set expectation data to match software renderer output
    tdd: refactor

---
+++ base/glversion_test.go
@@ -0,0 +1,19 @@
+package base_test
+
+import (
+	"testing"
+
+	"github.com/go-gl-legacy/gl"
+	"github.com/runningwild/glop/render/rendertest/testbuilder"
+)
+
+func TestGlVersion(t *testing.T) {
+	testbuilder.Run(func() {
+		versionString := gl.GetString(gl.VERSION)
+		t.Logf("versionString: %q\n", versionString)
+
+		if versionString == "" {
+			t.Error("gl.GetString(gl.VERSION) must not return the empty string once OpenGL is initialized")
+		}
+	})
+}
