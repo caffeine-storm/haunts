4999c6ec452e7f2584c896c1da31eb49779bf5c0
Thomas McKee <caffeine.storm@gmail.com>
2025-06-10 09:17:56 -0400

    refactor: put helpers where they're used and clean up after ourselves
    tdd: refactor

--- game/scenario_test.go
+++ game/scenario_test.go
@@ -1,6 +1,18 @@
 package game_test
 
-import "github.com/MobRulesGames/haunts/game"
+import (
+	"path/filepath"
+
+	"github.com/MobRulesGames/haunts/game"
+)
+
+func givenAScript() string {
+	return filepath.Join("versus", "basic.lua")
+}
+
+func givenAHouseName() string {
+	return "tutorial"
+}
 
 func givenAScenario() game.Scenario {
 	return game.Scenario{
