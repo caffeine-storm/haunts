77cae72ff3a889d071e112bb24432a9c32967942
Thomas McKee <caffeine.storm@gmail.com>
2025-08-14 14:06:30 -0400

    clean: remove some no-op code
    tdd: refactor

--- house/furniture.go
+++ house/furniture.go
@@ -4,7 +4,6 @@ import (
 	"github.com/MobRulesGames/haunts/base"
 	"github.com/MobRulesGames/haunts/texture"
 	"github.com/MobRulesGames/mathgl"
-	"github.com/go-gl-legacy/gl"
 )
 
 func MakeFurniture(name string) *Furniture {
@@ -123,17 +122,11 @@ func (f *Furniture) Color() (r, g, b, a byte) {
 }
 
 func (f *Furniture) Render(pos mathgl.Vec2, width float32) {
-	var rgba [4]float64
-	gl.GetDoublev(gl.CURRENT_COLOR, rgba[:])
-	gl.PushAttrib(gl.CURRENT_BIT)
 	if !f.Blocks_los || !f.alpha_enabled {
 		f.alpha = 1
 	}
-	var tff float64 = 255
-	gl.Color4ub(uint8(tff*rgba[0]), uint8(tff*rgba[1]), uint8(tff*rgba[2]), uint8(tff*rgba[3]*f.alpha))
 	orientation := f.Orientations[f.Rotation]
 	dy := width * float32(orientation.Texture.Data().Dy()) / float32(orientation.Texture.Data().Dx())
 	// orientation.Texture.Data().Render(float64(pos.X), float64(pos.Y), float64(width), float64(dy))
 	orientation.Texture.Data().RenderAdvanced(float64(pos.X), float64(pos.Y), float64(width), float64(dy), 0, !f.Flip)
-	gl.PopAttrib()
 }
