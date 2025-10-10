05aa9e2cf40aee1f61484a0ac897c362b5cdb8f1
Thomas McKee <caffeine.storm@gmail.com>
2025-10-10 14:52:57 -0400

    test: make sure we can draw the main bar
    tdd: red

---
+++ game/ui_main_bar_test.go
@@ -0,0 +1,32 @@
+package game_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/MobRulesGames/haunts/base"
+	"github.com/MobRulesGames/haunts/game"
+	"github.com/MobRulesGames/haunts/game/gametest"
+	"github.com/runningwild/glop/gui"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func givenAMainBar() gametest.Drawer {
+	aGame := gametest.GivenAGame()
+	mainBar, err := game.MakeMainBar(aGame)
+	if err != nil {
+		panic(fmt.Errorf("couldn't MakeMainBar: %w", err))
+	}
+
+	wrapper := &gui.AnchorBox{}
+	wrapper.AddChild(mainBar, gui.Anchor{})
+
+	return wrapper
+}
+
+func TestMainBar(t *testing.T) {
+	Convey("UI for the main bar", t, func(c C) {
+		base.SetDatadir("../data")
+		gametest.RunDrawingTest(c, givenAMainBar, "mainbar")
+	})
+}
