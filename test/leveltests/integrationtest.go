0e03cdb7ecb0cd650c8a436542c589b962de3bcc
Thomas McKee <caffeine.storm@gmail.com>
2025-10-18 10:08:10 -0400

    fix: draw things at the right size in end-to-end tests
    tdd: green-but-wip

--- test/leveltests/integrationtest.go
+++ test/leveltests/integrationtest.go
@@ -5,6 +5,12 @@ import (
 	"path"
 	"testing"
 
+	"github.com/MobRulesGames/haunts/base"
+	"github.com/MobRulesGames/haunts/game/gametest"
+	"github.com/MobRulesGames/haunts/globals"
+	"github.com/MobRulesGames/haunts/registry"
+	"github.com/MobRulesGames/haunts/texture"
+	"github.com/runningwild/glop/gui"
 	"github.com/runningwild/glop/render"
 	"github.com/runningwild/glop/render/rendertest"
 	"github.com/runningwild/glop/render/rendertest/testbuilder"
@@ -20,15 +26,6 @@ var (
 	ModePassNPlay ModeChoice = 1
 )
 
-type Tester interface {
-	ValidateExpectations(testcase string)
-}
-
-type renderingTester struct {
-	lvl         LevelChoice
-	renderQueue render.RenderQueueInterface
-}
-
 func testdataDir(lvl LevelChoice) string {
 	return map[LevelChoice]string{
 		Level1: "level1",
@@ -41,21 +38,73 @@ func testLabel(lvl LevelChoice) string {
 	}[lvl]
 }
 
+type Tester interface {
+	ValidateExpectations(testcase string)
+}
+
+type renderingTester struct {
+	lvl            LevelChoice
+	renderQueue    render.RenderQueueInterface
+	region         gui.Region
+	drawingContext gui.UpdateableDrawingContext
+}
+
 func (rt *renderingTester) ValidateExpectations(testcase string) {
 	expectedFile := rendertest.NewTestdataReference(path.Join(testdataDir(rt.lvl), testcase))
 
 	convey.So(rt.renderQueue, rendertest.ShouldLookLikeFile, expectedFile)
 }
 
+func (rt *renderingTester) Start() {
+	// Build a game.Game and game.GamePanel
+	gamePanel := gametest.GivenAGamePanel()
+
+	// TODO: Load the level
+
+	// TODO: Place units from the 'roster'
+
+	// Draw the UI
+	rt.renderQueue.Queue(func(render.RenderQueueState) {
+		gamePanel.Draw(rt.region, rt.drawingContext)
+	})
+}
+
+func (rt *renderingTester) End() {
+}
+
 func IntegrationTest(t *testing.T, level LevelChoice, mode ModeChoice, fn func(Tester)) {
 	testname := fmt.Sprintf("%s end-to-end test", testLabel(level))
-	testbuilder.Run(func(renderQueue render.RenderQueueInterface) {
+	region := gui.MakeRegion(0, 0, 1024, 750)
+	testbuilder.WithSize(region.Dx, region.Dy, func(renderQueue render.RenderQueueInterface) {
+
+		// TODO(tmckee:clean): this was lifted from gametest; DRY it out
+		base.SetDatadir("../../data")
+		globals.SetRenderQueue(renderQueue)
+		renderQueue.Queue(func(st render.RenderQueueState) {
+			globals.SetRenderQueueState(st)
+		})
+		renderQueue.Purge()
+
+		ctx := gametest.GivenADrawingContext(region.Dims)
+		registry.LoadAllRegistries()
+		base.InitDictionaries(ctx)
+		texture.Init(renderQueue)
+		base.InitShaders(renderQueue)
+		// TODO-end
+
 		convey.Convey(testname, t, func(conveyContext convey.C) {
 			tst := &renderingTester{
-				lvl:         level,
-				renderQueue: renderQueue,
+				lvl:            level,
+				renderQueue:    renderQueue,
+				region:         region,
+				drawingContext: ctx,
 			}
+
+			tst.Start()
+
 			fn(tst)
+
+			tst.End()
 		})
 	})
 }
